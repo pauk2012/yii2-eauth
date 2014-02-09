af6b4e40b7f9043795d8ebc85a0094b64e3a6a4d
Pavel Khimyack <paul.khimyack@gmail.com>
2014-02-09 20:43:33 +0200

    Revert "issue 12. wrong callbackUrl in ServiceBase"
    This reverts commit 5f9c626258a157730e557cdc30a5e04b8f76d791.

--- src/nodge/eauth/oauth/ServiceBase.php
+++ src/nodge/eauth/oauth/ServiceBase.php
@@ -96,7 +96,15 @@ abstract class ServiceBase extends \nodge\eauth\ServiceBase implements IAuthServ
 	 */
 	protected function getCallbackUrl() {
 		$request = Yii::$app->getRequest();
-        $url = $request->getAbsoluteUrl();
+		$url = $request->getHostInfo().$request->getBaseUrl().'/'.$request->getPathInfo();
+		$service = $request->getQueryParam('service');
+		if (isset($service)) {
+			// check if there is service name in query string (can also be in url rule)
+			$service = 'service='.urlencode($service);
+			if (strpos($request->getQueryString(), $service) !== false) {
+				$url .= '?'.$service;
+			}
+		}
 		return $url;
 	}
 
